0d7ae59f4b4ecde1ab9ec434ccdf209c8cc1f3ec
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-21 10:34:53 +0300

    karceva

---
+++ pages/testaboba/karceva.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import FormOpening from '~/components/form/FormOpening.vue'
+</script>
+
+<template>
+  <FormOpening />
+</template>
+
+<style scoped lang="scss">
+
+</style>
