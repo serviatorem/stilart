db0a6e45fcbfeff3a026ee4d1ce27ae0255adadf
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-11-16 02:28:08 +0300

    fix: fix api

--- API.ts
+++ API.ts
@@ -1,7 +1,7 @@
 export function useAPI () {
   return {
     async getPortfolio () {
-      const res = await fetch('https://admin.stilart-design.ru:1337/api/Portfolios?populate=*', {
+      const res = await fetch('https://admin.stilart-design.ru/api/Portfolios?populate=*', {
         method: 'GET'
       })
       return res.json()
